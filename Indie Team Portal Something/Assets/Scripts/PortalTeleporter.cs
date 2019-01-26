9df296fa79fd47f0e0f4552e0bb33b730562877b
mzolfo <32077843+mzolfo@users.noreply.github.com>
2019-01-26 17:20:36 -0600

    set up most of portal but doesnt work yet

---
+++ Indie Team Portal Something/Assets/Scripts/PortalTeleporter.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTeleporter : MonoBehaviour
+{
+    public Transform player;
+    public Transform reciever;
+
+    [SerializeField]
+    private bool playerIsOverlapping = false;
+    [SerializeField]
+    float exposeDotProduct;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (playerIsOverlapping)
+        {
+            Vector3 portalToPlayer = player.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+            exposeDotProduct = dotProduct;
+            // If this is true: The player has moved across the portal
+           // if (dotProduct < 0f)
+           // {
+                //Teleport him!
+                float rotationDiff = Quaternion.Angle(transform.rotation, reciever.rotation);
+                rotationDiff += 180;
+                player.Rotate(Vector3.up, rotationDiff);
+
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+                player.position = reciever.position + positionOffset;
+
+                playerIsOverlapping = false;
+            //}
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = false;
+        }
+    }
+}
---
+++ Indie Team Portal Something/Assets/Scripts/PortalTextureSetup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTextureSetup : MonoBehaviour
+{
+    public Camera cameraB;
+
+    public Material cameraMatB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (cameraB.targetTexture != null)
+        {
+            cameraB.targetTexture.Release();
+        }
+        cameraB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMatB.mainTexture = cameraB.targetTexture;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
