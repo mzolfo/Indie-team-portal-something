3b3fcb883c2bc111db6328d4f19ee5e3a0122c62
mzolfo <32077843+mzolfo@users.noreply.github.com>
2019-02-01 17:25:22 -0600

    comments in switcher

--- Indie Team Portal Something/Assets/Scripts/PortalSwitcher.cs
+++ Indie Team Portal Something/Assets/Scripts/PortalSwitcher.cs
@@ -7,7 +7,16 @@ public class PortalSwitcher : MonoBehaviour
 
     //we need to switch each aspect of a portal's destination and link during runtime
     //we want to be able to press a button or something and switch each portal to different destinations.
+    
 
+    //in the end i want to switch one stored either here or on each portal and be able to switch their destinations 
+    //and what they display
+
+    //what does it need to change these attributes
+    //access to the render plane and its script
+    //the render plane needs its cameramat switched to the target's cameramat
+    //access to the collision planes and their scripts
+    //colliders need the player's transform and the transform of the reciever
 
     // Start is called before the first frame update
     void Start()
