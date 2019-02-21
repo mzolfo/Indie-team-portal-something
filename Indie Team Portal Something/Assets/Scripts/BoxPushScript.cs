97e99b3091629f14c9093389ae56bfd6da8323a2
JWhittom <jawhittom@gmail.com>
2019-02-20 19:51:14 -0600

    Maintaining Changes
    Keeping incomplete changes in the saga of box pushing.

---
+++ Indie Team Portal Something/Assets/Scripts/BoxPushScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxPushScript : MonoBehaviour
+{
+    [SerializeField]
+    float pushForce = 5f;
+
+    private Vector3 force;
+    private CharacterController cc;
+
+    private void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Debug.Log("Bonk");
+        if(hit.rigidbody != null && !hit.rigidbody.isKinematic)
+        {
+            Debug.Log("push");
+            force = Vector3.Normalize(cc.velocity + transform.forward) * pushForce;
+            hit.rigidbody.AddForce(force);
+        }
+    }
+}
---
+++ Indie Team Portal Something/Assets/Scripts/PushableBox.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushableBox : MonoBehaviour
+{
+    [SerializeField]
+    float respawnThreshold = -20f;
+
+    private Rigidbody rb;
+    private Vector3 startingPoint;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        startingPoint = transform.position;
+    }
+
+    private void Update()
+    {
+        if (transform.position.y < respawnThreshold)
+            transform.position = startingPoint;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "DeliveryPoint")
+            rb.isKinematic = true;
+    }
+}
