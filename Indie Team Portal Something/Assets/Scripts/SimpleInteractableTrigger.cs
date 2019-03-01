5f075944e4835aca602a6401577e87a5e27a6382
mzolfo <32077843+mzolfo@users.noreply.github.com>
2019-02-28 19:37:19 -0600

    split some door animations

---
+++ Indie Team Portal Something/Assets/Scripts/SimpleInteractableTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleInteractableTrigger : MonoBehaviour
+{
+    //for now this only applies to the doors as we have them currently.
+    private int doorstate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
