3a5cabe0f4931cf763427a5da5b4b0a389ab602c
mzolfo <32077843+mzolfo@users.noreply.github.com>
2019-04-28 18:02:23 -0500

    fixed the stairwell in the divination tower and applied fixed doorframes in grand hall

---
+++ Indie Team Portal Something/Assets/Scripts/AttachPlayerToMovingStairs.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachPlayerToMovingStairs : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = null;
+        }
+    }
+}
